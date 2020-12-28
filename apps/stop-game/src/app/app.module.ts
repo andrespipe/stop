1fde457bbd63168bcde4d3bcacb5c835c08f6d38
andresm <andresm@blanclabs.com>
2020-12-28 14:21:43 -0500

    Initial UI

--- apps/stop-game/src/app/app.module.ts
+++ apps/stop-game/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainMenuComponent, MainFooterComponent, ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
